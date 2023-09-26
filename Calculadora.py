dd2b15fbd3b2e13ef3d7b36248c353de2a21efec
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2023-09-26 17:43:06 -0300

    Update Calculadora.py
    Transformando o metodo de calcular em uma função e adicionando a função again( ) para que o usuario possa usar  a calculadora multiplas vezes

--- Calculadora.py
+++ Calculadora.py
@@ -1,26 +1,52 @@
 
 
-operacao = input("Por favor digite a operação que deseja realizar: \n digite '+' para adição \n digite '-' para subtração \n digite '*' para multiplicação \n digite '/' para divisão \n")
+def calculate():
+    operacao = input("Por favor digite a operação que deseja realizar: \n digite '+' para adição \n digite '-' para subtração \n digite '*' para multiplicação \n digite '/' para divisão \n")
 
-number_1 = int(input('Digite o primeiro número: '))
-number_2 = int(input('Digite o segundo número: '))
+    number_1 = int(input('Digite o primeiro número: '))
+    number_2 = int(input('Digite o segundo número: '))
+    
+    
+    if operacao == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    
+    elif operacao == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    
+    elif operacao == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    
+    elif operacao == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    
+    else:
+        print('Você não digitou um operador válido, por favor rode o programa novamente.')
 
+    again()
+    
+    # Defina a função again() para perguntar ao usuario se ele quer usar novamente a calculadora
+def again():
 
-if operacao == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    # pegue o input do user
+    calc_again = input('''
+Você quer calcular de novo?
+Por favor digite S para SIM ou N para NAO.
+''')
 
-elif operacao == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    # Se o user digitar S, rode calculate() function
+    if calc_again.upper() == 'S':
+        calculate()
 
-elif operacao == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    # Se o user digitar N, diga adeus ao user e feche o programa
+    elif calc_again.upper() == 'N':
+        print('Muito bem, obrigado por usar a calculadora')
 
-elif operacao == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
-
-else:
-    print('Você não digitou um operador válido, por favor rode o programa novamente.')
+    # Se o user digitar outra letra, rode a function de novo
+    else:
+        again()
+        
+calculate()
